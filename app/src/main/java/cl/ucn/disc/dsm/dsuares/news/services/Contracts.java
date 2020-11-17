95c0efc72bc8c9c1b989ac4f6749683921405248
arkanoid4 <daniel.suares@alumnos.ucn.cl>
2020-11-17 11:26:46 -0300

    feat(services/Contracts): add instance save news

--- app/src/main/java/cl/ucn/disc/dsm/dsuares/news/services/Contracts.java
+++ app/src/main/java/cl/ucn/disc/dsm/dsuares/news/services/Contracts.java
@@ -27,4 +27,9 @@ public interface Contracts {
      */
     List<News> retrieveNews(Integer size);
 
+    /**
+     * Save one News into the System.
+     * @param news to save.
+     */
+    void saveNews(News news);
 }
